662a8b89ff993e9c0d931c3efde08a179ac8ab9a
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-09-15 23:18:33 -0300

    Desafio

---
+++ src/config/upload.ts
@@ -0,0 +1,20 @@
+import { Request } from 'express';
+import multer from 'multer';
+import path from 'path';
+import crypto from 'crypto';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  directory: tmpFolder,
+
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request: Request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
